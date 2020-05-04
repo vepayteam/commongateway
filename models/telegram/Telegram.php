df4ad165d7f7a7df750a1a19deeb8c66db47e282
ayvepay <ayuriev@vepay.online>
2020-05-04 14:56:29 +0300

    upd

--- models/telegram/Telegram.php
+++ models/telegram/Telegram.php
@@ -21,7 +21,7 @@ class Telegram
         if (is_resource($process)) {
             fwrite($pipes[0], "\r\n");
             fclose($pipes[0]);
-            $read = $pipes[1];
+            $read = [$pipes[1]];
             $write  = NULL;
             $except = NULL;
             if (stream_select($read, $write, $except, 0) !== false) {
