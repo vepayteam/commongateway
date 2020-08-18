764f8a8bfc2e3394b36f64b9cfa359b81d98efbc
Vadims I <vivolgin@vepay.online>
2020-08-18 11:43:39 +0300

    Revert "feat: VPBC-132 Доработка расщепления платежей"
    This reverts commit 59626a56accf66e611e79d5a1a26f0e924e782da.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -26,7 +26,7 @@ return array(
     'toriphes\\console\\' => array($vendorDir . '/toriphes/yii2-console-runner'),
     'setasign\\Fpdi\\' => array($vendorDir . '/setasign/fpdi/src'),
     'qfsx\\yii2\\curl\\' => array($vendorDir . '/qfsx/yii2-curl/src'),
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/type-resolver/src', $vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
     'kartik\\mpdf\\' => array($vendorDir . '/kartik-v/yii2-mpdf/src'),
     'cebe\\markdown\\' => array($vendorDir . '/cebe/markdown'),
     'bestyii\\openapiReader\\' => array($vendorDir . '/bestyii/yii2-openapi-reader/src'),
@@ -86,5 +86,5 @@ return array(
     'Complex\\' => array($vendorDir . '/markbaker/complex/classes/src'),
     'Codeception\\PHPUnit\\' => array($vendorDir . '/codeception/phpunit-wrapper/src'),
     'Codeception\\Extension\\' => array($vendorDir . '/codeception/codeception/ext'),
-    'Codeception\\' => array($vendorDir . '/codeception/stub/src', $vendorDir . '/codeception/codeception/src/Codeception', $vendorDir . '/codeception/verify/src/Codeception'),
+    'Codeception\\' => array($vendorDir . '/codeception/codeception/src/Codeception', $vendorDir . '/codeception/stub/src', $vendorDir . '/codeception/verify/src/Codeception'),
 );
