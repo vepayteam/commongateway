24cadcfcb339df5ff9a31ddce7db906086535a36
Magomed Gadzhiev <magadzhiev@velay.online>
2020-11-25 12:40:13 +0300

    fix: VPBC-273 Ошибка баланса по разбивке

--- modules/partner/views/stat/ident.php
+++ modules/partner/views/stat/ident.php
@@ -13,7 +13,7 @@
 
 use app\models\partner\UserLk;
 use app\services\balance\models\PartsBalanceForm;
-use app\services\ident\models\IdentStatisticForm;
+use app\services\ident\forms\IdentStatisticForm;
 use yii\web\View;
 
 $this->title = "Баланс по разбивке (Платформа)";
