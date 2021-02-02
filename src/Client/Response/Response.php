69380fc2803e6f975ad12a439bdb882bf5c11229
Kuzob, Evgeny <e.kuzob@itransition.com>
2021-02-02 16:30:35 +0300

    [VEPAY-4][feat][backend] - update Response

--- src/Client/Response/Response.php
+++ src/Client/Response/Response.php
@@ -5,39 +5,15 @@ namespace Vepay\Gateway\Client\Response;
 class Response implements ResponseInterface
 {
     protected string $raw;
-    protected string $status;
-    protected string $message;
-    protected string $content;
-
-    public function setStatus(string $status): ResponseInterface
-    {
-        $this->status = $status;
-
-        return $this;
-    }
 
     public function getStatus(): ?string
     {
         return null;
     }
 
-    public function setMessage(string $message): ResponseInterface
-    {
-        $this->message = $message;
-
-        return $this;
-    }
-
     public function getMessage(): ?string
     {
         return null;
-    }
-
-    public function setContent(string $content): ResponseInterface
-    {
-        $this->content = $content;
-
-        return $this;
     }
 
     public function getContent(): string
