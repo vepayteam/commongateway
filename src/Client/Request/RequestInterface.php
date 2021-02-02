d422b5e87400b77544de8a32523a739846fb9657
Kuzob, Evgeny <e.kuzob@itransition.com>
2021-02-02 15:52:39 +0300

    [VEPAY-4][feat][backend] - refactor RequestInterface

--- src/Client/Request/RequestInterface.php
+++ src/Client/Request/RequestInterface.php
@@ -6,11 +6,21 @@ interface RequestInterface
 {
     public function setEndpoint(string $endpoint): RequestInterface;
 
+    public function getEndpoint(): string;
+
     public function setMethod(string $method): RequestInterface;
+
+    public function getMethod(): string;
 
     public function setHeaders(array $headers): RequestInterface;
 
+    public function getHeaders(): array;
+
     public function setParameters(array $parameters): RequestInterface;
 
+    public function getParameters(): array;
+
     public function setMiddlewares(array $middlewares): RequestInterface;
+
+    public function getMiddlewares(): array;
 }
