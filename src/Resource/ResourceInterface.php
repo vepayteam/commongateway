17f1d60a743ea0295485940cd49b9c8426a06b64
Kuzob, Evgeny <e.kuzob@itransition.com>
2021-02-02 12:03:00 +0300

    [VEPAY-4][feat][backend] - refactor ResourceInterface

--- src/Resource/ResourceInterface.php
+++ src/Resource/ResourceInterface.php
@@ -2,12 +2,11 @@
 
 namespace Vepay\Gateway\Resource;
 
-use Vepay\Gateway\Client\ClientConfiguratorInterface;
 use Vepay\Gateway\Client\ClientInterface;
 
 interface ResourceInterface
 {
     public function setClient(ClientInterface $client);
 
-    public function getClient(ClientConfiguratorInterface $clientConfigurator): ClientInterface;
+    public function getClient(): ClientInterface;
 }
