38f19a659606343ec9ed0127f91d71b7b9b3ef41
Kuzob, Evgeny <e.kuzob@itransition.com>
2021-02-03 10:32:16 +0300

    [VEPAY-4][feat][backend] - update Request

--- src/Client/Request/RequestInterface.php
+++ src/Client/Request/RequestInterface.php
@@ -2,6 +2,8 @@
 
 namespace Vepay\Gateway\Client\Request;
 
+use Closure;
+
 interface RequestInterface
 {
     public function setEndpoint(string $endpoint): RequestInterface;
@@ -14,13 +16,19 @@ interface RequestInterface
 
     public function setHeaders(array $headers): RequestInterface;
 
+    public function addHeader(string $header, string $value): RequestInterface;
+
     public function getHeaders(): array;
 
     public function setParameters(array $parameters): RequestInterface;
 
+    public function addParameter(string $name, string $value): RequestInterface;
+
     public function getParameters(): array;
 
     public function setMiddlewares(array $middlewares): RequestInterface;
+
+    public function addMiddleware(string $name, Closure $closure): RequestInterface;
 
     public function getMiddlewares(): array;
 }
