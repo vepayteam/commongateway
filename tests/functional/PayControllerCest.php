2dd966231487d495acc4e43a7dba9a3ca7e8fc4b
Anatoliy Smirnov <anatoliysmirnov79@yandex.ru>
2021-02-11 15:08:57 +0300

    feature/VPBC-347 tests/functional/PayControllerCest.php

---
+++ tests/functional/PayControllerCest.php
@@ -0,0 +1,85 @@
+<?php 
+
+class PayControllerCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function tryToFormDataTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/form-data', ['id' => 0]);
+        $I->see('Not Found: Счет для оплаты не найден');
+    }
+
+    public function tryToSaveDataTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/save-data', ['id' => 0]);
+        $I->see('VEPAY - VEPAY v 1.0.7 Сервис VEPAY.');
+        $I->seeCurrentUrlEquals('/pay/form/0');
+    }
+
+    public function tryToFormTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/form', ['id' => 0]);
+        $I->see('Not Found: Счет для оплаты не найден');
+    }
+
+    public function tryToCreatepayTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/createpay');
+        $I->see(' Not Found:');
+    }
+
+    public function tryToDeclinepayTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/declinepay');
+        $I->see(' Not Found:');
+    }
+
+    public function tryToOrderdoneTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/orderdone', ['id' => 0]);
+        $I->see(' Not Found:');
+    }
+
+    public function tryToOrderokTest(FunctionalTester $I)
+    {
+//        TODO-test Долго работает
+//        $I->amOnRoute('pay/orderok', ['id' => 0]);
+//        $I->see(' Not Found:');
+    }
+
+    public function tryToOrderPrintTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/order-print', ['id' => 0]);
+        $I->see(' Not Found:');
+    }
+
+    public function tryToOrderInvoiceTest(FunctionalTester $I)
+    {
+        $I->amOnRoute('pay/order-invoice', ['id' => 0]);
+        $I->see(' Not Found:');
+    }
+
+//    public function tryToApplepayvalidateTest(FunctionalTester $I)
+//    {
+//
+//    }
+
+//    public function tryToApplepaycreateTest(FunctionalTester $I)
+//    {
+//
+//    }
+
+//    public function tryToGooglepaycreateTest(FunctionalTester $I)
+//    {
+//
+//    }
+
+//    public function tryToSamsungpaycreateTest(FunctionalTester $I)
+//    {
+//
+//    }
+
+}
