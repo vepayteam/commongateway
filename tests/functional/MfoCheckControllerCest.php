d332f2bdddea45cacf20af79cc308c01d95c6c9f
Anatoliy Smirnov <anatoliysmirnov79@yandex.ru>
2021-02-19 14:43:49 +0300

    feature/VPBC-347

--- tests/functional/MfoCheckControllerCest.php
+++ tests/functional/MfoCheckControllerCest.php
@@ -9,7 +9,7 @@ class MfoCheckControllerCest
     public function tryToIndexTest(FunctionalTester $I)
     {
         $I->haveHttpHeader('X-Mfo', 117);
-        $I->haveHttpHeader('X-Token', '4db602f436fda086d9b946267dcf0959197779cb');
+        $I->haveHttpHeader('X-Token', '085c939f7161f9aee0d649c93062e0740d6af744');
         $I->amOnRoute('mfo/check');
         $I->see('{"status":1}');
     }
--- tests/functional/MfoOutControllerCest.php
+++ tests/functional/MfoOutControllerCest.php
@@ -9,7 +9,7 @@ class MfoOutControllerCest
     public function tryToPaycardTest(FunctionalTester $I)
     {
         $I->haveHttpHeader('X-Mfo', 117);
-        $I->haveHttpHeader('X-Token', '4db602f436fda086d9b946267dcf0959197779cb');
+        $I->haveHttpHeader('X-Token', '085c939f7161f9aee0d649c93062e0740d6af744');
         $I->amOnRoute('mfo/out/paycard');
         $I->see('{"status":0,"message":"Ид карты или номер карты обязательны к заполнению"}');
     }
@@ -17,7 +17,7 @@ class MfoOutControllerCest
     public function tryToPayaccTest(FunctionalTester $I)
     {
         $I->haveHttpHeader('X-Mfo', 117);
-        $I->haveHttpHeader('X-Token', '4db602f436fda086d9b946267dcf0959197779cb');
+        $I->haveHttpHeader('X-Token', '085c939f7161f9aee0d649c93062e0740d6af744');
         $I->amOnRoute('mfo/out/payacc');
         $I->see('{"status":0,"message":"Fio cannot be blank."}');
     }
@@ -25,7 +25,7 @@ class MfoOutControllerCest
     public function tryToPayulTest(FunctionalTester $I)
     {
         $I->haveHttpHeader('X-Mfo', 117);
-        $I->haveHttpHeader('X-Token', '4db602f436fda086d9b946267dcf0959197779cb');
+        $I->haveHttpHeader('X-Token', '085c939f7161f9aee0d649c93062e0740d6af744');
         $I->amOnRoute('mfo/out/payul');
         $I->see('{"status":0,"message":"Name cannot be blank."}');
     }
@@ -33,7 +33,7 @@ class MfoOutControllerCest
     public function tryToStateTest(FunctionalTester $I)
     {
         $I->haveHttpHeader('X-Mfo', 117);
-        $I->haveHttpHeader('X-Token', '4db602f436fda086d9b946267dcf0959197779cb');
+        $I->haveHttpHeader('X-Token', '085c939f7161f9aee0d649c93062e0740d6af744');
         $I->amOnRoute('mfo/out/state');
         $I->see('{"status":0,"message":"Счет не найден"}');
     }
