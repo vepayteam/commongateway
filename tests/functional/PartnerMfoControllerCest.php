20f8eebb61169eebef18e266f5e9fe4818c0eaf4
npchelnikov <npchelnikov@vepay.online>
2021-04-22 13:59:23 +0300

    feat: VPBC-177. Доработка тестов, проверяем доступ к балансу по разбивке для разных пользователей

--- tests/functional/PartnerMfoControllerCest.php
+++ tests/functional/PartnerMfoControllerCest.php
@@ -2,6 +2,8 @@
 
 use app\models\partner\PartnerUsers;
 use app\models\partner\UserLk;
+use app\models\payonline\Uslugatovar;
+
 //use app\services\balance\models\PartsBalancePartnerForm;
 
 class PartnerMfoControllerCest
@@ -20,43 +22,104 @@ class PartnerMfoControllerCest
             ->andWhere(['or', 'partner.ID > 0', 'partner_users.IsAdmin = 1'])
             ->one();
 
-        if ($partner) {
-            $user = new UserLk();
-            $userLkReflectionClass = new ReflectionClass(UserLk::class);
-            $id = $userLkReflectionClass->getProperty('id');
-            $id->setAccessible(true);
-            $id->setValue($user, $partner->Login);
-            $fio = $userLkReflectionClass->getProperty('fio');
-            $fio->setAccessible(true);
-            $fio->setValue($user, $partner->FIO);
-            $password = $userLkReflectionClass->getProperty('password');
-            $password->setAccessible(true);
-            $password->setValue($user, $partner->Password);
-            $isAdmin = $userLkReflectionClass->getProperty('isAdmin');
-            $isAdmin->setAccessible(true);
-            $isAdmin->setValue($user, $partner->IsAdmin);
-            $roleUser = $userLkReflectionClass->getProperty('roleUser');
-            $roleUser->setAccessible(true);
-            $roleUser->setValue($user, $partner->RoleUser);
-            $idPartner = $userLkReflectionClass->getProperty('partner');
-            $idPartner->setAccessible(true);
-            $idPartner->setValue($user, $partner->IdPartner);
-            $IdUser = $userLkReflectionClass->getProperty('IdUser');
-            $IdUser->setAccessible(true);
-            $IdUser->setValue($user, $partner->ID);
-            $partnerModel = $userLkReflectionClass->getProperty('partnerModel');
-            $partnerModel->setAccessible(true);
-            $partnerModel->setValue($user, $partner);
-            return $user;
-        }
-        return null;
+        return $this->getUser($partner);
     }
 
-
-    public function tryToPartsBalanceTest(FunctionalTester $I)
+    private function findAdminIdentity()
     {
+        /** @var PartnerUsers $partner */
+        $partner = PartnerUsers::find()
+            ->where(['partner_users.IsDeleted' => 0, 'partner_users.IsActive' => 1, 'partner_users.IsAdmin' => 1])
+            ->leftJoin('partner', 'partner.ID = partner_users.IdPartner AND partner.IsDeleted = 0 AND partner.IsBlocked = 0')
+            ->one();
+
+        return $this->getUser($partner);
+    }
+
+    public function findPartnerWithPartsBalanceIdentity()
+    {
+        /** @var PartnerUsers $partner */
+        $partner = PartnerUsers::find()
+            ->where(['partner_users.IsDeleted' => 0, 'partner_users.IsActive' => 1, 'partner_users.IsAdmin' => 0])
+            ->leftJoin('partner', 'partner.ID = partner_users.IdPartner AND partner.IsDeleted = 0 AND partner.IsBlocked = 0')
+            ->leftJoin('uslugatovar', 'uslugatovar.IDPartner = partner_users.IdPartner')
+            ->where(['in', 'uslugatovar.IsCustom', Uslugatovar::$PartsBalanceAccessCustoms])
+            ->one();
+
+        return $this->getUser($partner);
+    }
+
+    public function findPartnerWithoutPartsBalanceIdentity()
+    {
+        /** @var PartnerUsers $partner */
+        $partner = PartnerUsers::find()
+            ->where(['partner_users.IsDeleted' => 0, 'partner_users.IsActive' => 1, 'partner_users.IsAdmin' => 0])
+            ->leftJoin('partner', 'partner.ID = partner_users.IdPartner AND partner.IsDeleted = 0 AND partner.IsBlocked = 0')
+            ->leftJoin('uslugatovar', 'uslugatovar.IDPartner = partner_users.IdPartner')
+            ->where(['not in', 'uslugatovar.IsCustom', Uslugatovar::$PartsBalanceAccessCustoms])
+            ->one();
+
+        return $this->getUser($partner);
+    }
+
+    private function getUser(PartnerUsers $partner): ?UserLk
+    {
+        if (!$partner) {
+            return null;
+        }
+
+        $user = new UserLk();
+        $userLkReflectionClass = new ReflectionClass(UserLk::class);
+        $id = $userLkReflectionClass->getProperty('id');
+        $id->setAccessible(true);
+        $id->setValue($user, $partner->Login);
+        $fio = $userLkReflectionClass->getProperty('fio');
+        $fio->setAccessible(true);
+        $fio->setValue($user, $partner->FIO);
+        $password = $userLkReflectionClass->getProperty('password');
+        $password->setAccessible(true);
+        $password->setValue($user, $partner->Password);
+        $isAdmin = $userLkReflectionClass->getProperty('isAdmin');
+        $isAdmin->setAccessible(true);
+        $isAdmin->setValue($user, $partner->IsAdmin);
+        $roleUser = $userLkReflectionClass->getProperty('roleUser');
+        $roleUser->setAccessible(true);
+        $roleUser->setValue($user, $partner->RoleUser);
+        $idPartner = $userLkReflectionClass->getProperty('partner');
+        $idPartner->setAccessible(true);
+        $idPartner->setValue($user, $partner->IdPartner);
+        $IdUser = $userLkReflectionClass->getProperty('IdUser');
+        $IdUser->setAccessible(true);
+        $IdUser->setValue($user, $partner->ID);
+        $partnerModel = $userLkReflectionClass->getProperty('partnerModel');
+        $partnerModel->setAccessible(true);
+        $partnerModel->setValue($user, $partner);
+
+        return $user;
+    }
+
+    public function tryToPartsBalanceAdminTest(FunctionalTester $I)
+    {
+        $I->amLoggedInAs($this->findAdminIdentity());
+
         $I->amOnRoute('partner/mfo/parts-balance');
         $I->see('Баланс по разбивке (Платформа)');
+    }
+
+    public function tryToPartsBalanceTruePartnerTest(FunctionalTester $I)
+    {
+        $I->amLoggedInAs($this->findPartnerWithPartsBalanceIdentity());
+
+        $I->amOnRoute('partner/mfo/parts-balance');
+        $I->see('Баланс по разбивке (Платформа)');
+    }
+
+    public function tryToPartsBalanceFalsePartnerTest(FunctionalTester $I)
+    {
+        $I->amLoggedInAs($this->findPartnerWithoutPartsBalanceIdentity());
+
+        $I->amOnRoute('partner/mfo/parts-balance');
+        $I->seeCurrentUrlEquals('/partner');
     }
 
 //    public function tryToPartsBalanceProcessingTest(FunctionalTester $I)
@@ -64,10 +127,28 @@ class PartnerMfoControllerCest
 //
 //    }
 
-    public function tryToPartsBalancePartnerTest(FunctionalTester $I)
+    public function tryToPartsBalancePartnerAdminTest(FunctionalTester $I)
     {
+        $I->amLoggedInAs($this->findAdminIdentity());
+
         $I->amOnRoute('partner/mfo/parts-balance-partner');
         $I->see('Баланс по разбивке (Партнер)');
+    }
+
+    public function tryToPartsBalancePartnerTrueTest(FunctionalTester $I)
+    {
+        $I->amLoggedInAs($this->findPartnerWithPartsBalanceIdentity());
+
+        $I->amOnRoute('partner/mfo/parts-balance-partner');
+        $I->see('Баланс по разбивке (Партнер)');
+    }
+
+    public function tryToPartsBalancePartnerFalseTest(FunctionalTester $I)
+    {
+        $I->amLoggedInAs($this->findPartnerWithoutPartsBalanceIdentity());
+
+        $I->amOnRoute('partner/mfo/parts-balance-partner');
+        $I->seeCurrentUrlEquals('/partner');
     }
 
 //    public function tryToPartsBalancePartnerProcessingTest(FunctionalTester $I)
