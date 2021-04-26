2eae32e05057a6f8a5d685705329e5befd74fea0
Никита Черненко <nikitozina@inbox.ru>
2021-04-26 10:25:30 +0300

    VPBC-704. Провести рефакторинг Статистика - Регулярные платежи

--- models/partner/stat/AutopayStat.php
+++ models/partner/stat/AutopayStat.php
@@ -43,7 +43,6 @@ class AutopayStat extends Model
         }
 
         $ret = [
-            'cntcards' => 0,
             'cntnewcards' => 0,
             'activecards' => 0,
             'reqonecard' => 0,
@@ -51,19 +50,6 @@ class AutopayStat extends Model
             'payscards' => 0,
             'sumpayscards ' => 0
         ];
-
-        //видеть общее количество привязанных карт
-        $query = new Query();
-        $query
-            ->select(['c.ID'])
-            ->from('`cards` AS c')
-            ->leftJoin('user AS u', 'u.ID = c.IdUser')
-            ->where(['c.TypeCard' => 0]);
-        if ($IdPart > 0) {
-            $query->andWhere('u.ExtOrg = :IDPARTNER', [':IDPARTNER' => $IdPart]);
-        }
-        $countResult = (new Query())->select(['count' => 'COUNT(ID)'])->from('cards')->where(['in', 'ID', $query])->cache(30)->one();
-        $ret['cntcards'] = $countResult['count'];
 
         //Количество новых карт
         $query = new Query();
