ba5fb7857fc70de792ca068b67fdac874b6a70b9
Magomed Gadzhiev <magadzhiev@velay.online>
2021-04-27 09:10:38 +0300

    feat: VPBC-740 Forta Tech / Зависание отказных выплат в обработке

--- services/payment/banks/FortaTechAdapter.php
+++ services/payment/banks/FortaTechAdapter.php
@@ -179,21 +179,35 @@ class FortaTechAdapter implements IBankAdapter
         $checkStatusPayResponse = new CheckStatusPayResponse();
 
         if($ans['status'] == true && isset($ans['data']['cards'][0]['transferParts'])) {
-            $checkStatusPayResponse->status = BaseResponse::STATUS_DONE;
+            // TODO: refact
 
+
+            $transferParts = $ans['data']['cards'][0]['transferParts'];
             $errorData = '';
-            foreach ($ans['data']['cards'][0]['transferParts'] as $transferPart) {
+            $errorsCount = 0;
+            $initCount = 0;
+            $paidCount = 0;
+            foreach ($transferParts as $transferPart) {
                 if($transferPart['status'] == 'STATUS_ERROR') {
-                    $checkStatusPayResponse->status = BaseResponse::STATUS_CREATED;
-                    $errorData .= Json::encode($transferPart) . "\n";
+                    $errorsCount++;
                 } elseif ($transferPart['status'] == 'STATUS_INIT') {
-                    $checkStatusPayResponse->status = BaseResponse::STATUS_CREATED;
-                } elseif (
-                    $transferPart['status'] == 'STATUS_PAID'
-                    && $checkStatusPayResponse->status == BaseResponse::STATUS_CREATED
-                ) {
-                    continue;
+                    $initCount++;
+                } elseif ($transferPart['status'] == 'STATUS_PAID') {
+                    $paidCount++;
                 }
+            }
+
+            if(count($transferParts) === $paidCount) {
+                // если все части платежей успешны
+                $checkStatusPayResponse->status = BaseResponse::STATUS_DONE;
+            } elseif (count($transferParts) === $errorsCount) {
+                // если все части платежей с ошибкой
+                $checkStatusPayResponse->status = BaseResponse::STATUS_ERROR;
+            } elseif ($initCount === 0) {
+                // если части платежей с ошибками и успешны
+                $checkStatusPayResponse->status = BaseResponse::STATUS_ERROR;
+            } else {
+                $checkStatusPayResponse->status = BaseResponse::STATUS_CREATED;
             }
         } else {
             $checkStatusPayResponse->status = BaseResponse::STATUS_ERROR;
