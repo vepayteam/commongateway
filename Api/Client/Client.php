c24559e2d331085f5dbc66ae1a301973f24ff3f2
tdundurs <tdundurs@vepay.online>
2021-05-19 12:39:44 +0300

    feat: VPBC-769 pay/lk client data validation, email validator add, refactor

--- Api/Client/Client.php
+++ Api/Client/Client.php
@@ -4,8 +4,10 @@ namespace app\Api\Client;
 
 final class Client extends AbstractClient
 {
-    public function __construct(array $clientConfig = [], $logInfoMessage = '')
-    {
+    public function __construct(
+        array $clientConfig = [],
+        string $logInfoMessage = ""
+    ) {
         parent::__construct($clientConfig, $logInfoMessage);
     }
 }
