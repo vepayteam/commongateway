c60e399acc0427f2f61241e16a5a7598900e9ab5
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2021-08-09 10:32:07 +0300

    fix: VPBC-1368 remove commented code

--- web/aassets/js/partner_gate_settings.js
+++ web/aassets/js/partner_gate_settings.js
@@ -1,25 +1,3 @@
-// (function ($) {
-//
-//     "use strict";
-//
-//     $('#partner-edit__bank-gates-edit-modal__gate-form')
-//         .find('select[name=FeeCurrencyId]')
-//         .on('change', function (e) {
-//             console.log(e)
-//             try {
-//                 const chosenCurrencyElements = $('#comissionWrapper .chosenCurrency')
-//                 const currentCurrency = e.target.options[e.target.options.selectedIndex]?.innerText
-//                 chosenCurrencyElements.text(currentCurrency || '')
-//             } catch (e) {
-//                 console.log('ошибка изменения валюты', e)
-//             }
-//
-//         })
-//
-//
-// }(jQuery || $));
-//
-
 const form = document.querySelector('#partner-edit__bank-gates-edit-modal__gate-form')
 const fixedComissionCurrency = form.querySelector('select[name=FeeCurrencyId]')
 const minimalComissionCurrency = form.querySelector('select[name=MinimalFeeCurrencyId]')
