20593bd8a8b276291da1ec21248e2a0a78a97e31
Кирилл Большаков <kbolshakov@vepay.online>
2021-09-05 22:45:49 +0300

    feat: VPBC-855 ЛК / Доработка раздела Отчеты фикс вёрстки

--- modules/partner/views/stat/otch.php
+++ modules/partner/views/stat/otch.php
@@ -22,7 +22,8 @@ $this->params['breadcrumbs'][] = $this->params['breadtitle'];
             </div>
             <div class="ibox-content">
                 <form class="form-horizontal" id="otchlistform">
-                    <div class="form-group"><label class="col-sm-2 control-label">Дата</label>
+                    <div class="form-group">
+	                    <label class="col-sm-2 control-label">Дата</label>
                         <div class="col-sm-4">
                             <div class="input-daterange input-group">
                                 <input type="text" name="datefrom" value="<?=date("d.m.Y")?> 00:00" maxlength="10" class="form-control">
@@ -30,50 +31,51 @@ $this->params['breadcrumbs'][] = $this->params['breadtitle'];
                                 <input type="text" name="dateto" value="<?=date("d.m.Y")?> 23:59" maxlength="10" class="form-control">
                             </div>
                         </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Провайдер</label>
+                        <div class="col-sm-4">
+                            <select class="form-control multiselect-field" multiple name="idBank[]">
+                                <?php foreach ($bankList as $bank) : ?>
+                                    <option value="<?= $bank->ID ?>"><?= $bank->Name ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <?php if ($IsAdmin) : ?>
                         <div class="form-group">
-                            <label class="col-sm-2 control-label">Провайдер</label>
+                            <label class="col-sm-2 control-label">Мерчант</label>
                             <div class="col-sm-4">
-                                <select class="form-control multiselect-field" multiple name="idBank[]">
-                                    <?php foreach ($bankList as $bank) : ?>
-                                        <option value="<?= $bank->ID ?>"><?= $bank->Name ?></option>
+                                <select class="form-control" name="IdPart">
+                                    <option value="-1" data-ismfo="-1">Все</option>
+                                    <?php foreach ($partnerlist as $partn) : ?>
+                                        <option value="<?= $partn->ID ?>"
+                                            data-ismfo="<?= $partn->ID == 1 ? 2 : $partn->IsMfo ?>"><?= $partn->Name ?></option>
                                     <?php endforeach; ?>
                                 </select>
                             </div>
                         </div>
-                        <?php if ($IsAdmin) : ?>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label">Мерчант</label>
-                                <div class="col-sm-4">
-                                    <select class="form-control" name="IdPart">
-                                        <option value="-1" data-ismfo="-1">Все</option>
-                                        <?php foreach ($partnerlist as $partn) : ?>
-                                            <option value="<?= $partn->ID ?>"
-                                                data-ismfo="<?= $partn->ID == 1 ? 2 : $partn->IsMfo ?>"><?= $partn->Name ?></option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div>
-                            </div>
-                        <?php endif; ?>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">Тип операции</label>
-                            <div class="col-sm-4">
-                                <select class="form-control multiselect-field" multiple name="TypeUslug[]">
-                                    <?php foreach ($uslugilist as $usl) : ?>
-                                        <option value="<?= $usl->ID ?>" data-partner="<?= $usl->IsMfo ?>"><?= $usl->Name ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
+                    <?php endif; ?>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Тип операции</label>
+                        <div class="col-sm-4">
+                            <select class="form-control multiselect-field" multiple name="TypeUslug[]">
+                                <?php foreach ($uslugilist as $usl) : ?>
+                                    <option value="<?= $usl->ID ?>" data-partner="<?= $usl->IsMfo ?>"><?= $usl->Name ?></option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
-                        <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-4">
-                                <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
-                                <button class="btn btn-sm btn-primary" type="submit">Сформировать</button>
-                            </div>
-                            <div class="col-sm-offset-2 col-sm-4">
-                                <a href="/partner/stat/otchetps" target="_blank" class="btn btn-sm btn-default" id="otchetpsxls">Отчет по ПС (xls)</a>
-                            </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-4">
+                            <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
+                            <button class="btn btn-sm btn-primary" type="submit">Сформировать</button>
                         </div>
-                    </form>
+                        <div class="col-sm-offset-2 col-sm-4">
+                            <a href="/partner/stat/otchetps" target="_blank" class="btn btn-sm btn-default" id="otchetpsxls">Отчет по ПС (xls)</a>
+                        </div>
+                    </div>
+                </form>
 
                 <div class="sk-spinner sk-spinner-wave">
                     <div class="sk-rect1"></div>
