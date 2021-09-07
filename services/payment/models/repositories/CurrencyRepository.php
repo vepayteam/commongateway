e30a0edea5bb56f92af559ec119452b243243fd8
Кирилл Большаков <kbolshakov@vepay.online>
2021-09-07 06:12:05 +0300

    fix:VPBC-972 Очень много дублей в запросах с валютой - требуется оптимизация Обновлённый новый репозиторий :) V2.0

--- services/payment/models/repositories/CurrencyRepository.php
+++ services/payment/models/repositories/CurrencyRepository.php
@@ -16,14 +16,6 @@ class CurrencyRepository
      * @var Currency[]
      */
     private static $currencies;
-    /**
-     * @var Currency[]
-     */
-    private static $currenciesById;
-    /**
-     * @var Currency[]
-     */
-    private static $currenciesByCode;
 
     /**
      * @return Currency[]
@@ -41,10 +33,7 @@ class CurrencyRepository
      */
     public static function getCurrenciesById(): array
     {
-        if (!self::$currenciesById) {
-            self::$currenciesById = ArrayHelper::index(self::getCurrencies(), 'Id');
-        }
-        return self::$currenciesById;
+        return ArrayHelper::index(self::getCurrencies(), 'Id');
     }
 
     /**
@@ -52,10 +41,7 @@ class CurrencyRepository
      */
     public static function getCurrenciesByCode(): array
     {
-        if (!self::$currenciesByCode) {
-            self::$currenciesByCode = ArrayHelper::index(self::getCurrencies(), 'Code');
-        }
-        return self::$currenciesByCode;
+        return ArrayHelper::index(self::getCurrencies(), 'Code');
     }
 
     public static function getCurrencyById(int $id): ?Currency
@@ -73,7 +59,7 @@ class CurrencyRepository
      */
     public static function getAll(): array
     {
-        return self::getCurrenciesById();
+        return self::getCurrencies();
     }
 
     public static function getCurrencyCodeById(int $id): ?Currency
@@ -95,7 +81,7 @@ class CurrencyRepository
     public function getCurrency(string $code = null, int $id = null)
     {
         if (!$code && !$id) {
-            return self::getCurrenciesById();
+            return self::getCurrencies();
         }
         $code = $code ? self::getCurrencyByCode($code) : $code;
         $id = $id ? self::getCurrencyById($id) : $id;
