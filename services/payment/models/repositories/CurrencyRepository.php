29e4102d8691f6595786dbf260082eb69f05a5f8
Кирилл Большаков <kbolshakov@vepay.online>
2021-09-07 06:04:32 +0300

    fix:VPBC-972 Очень много дублей в запросах с валютой - требуется оптимизация Полностью переработанный репозитарий для работы с валютами. Делает всего один запрос в базу за всё время сущестования. При этом сохранилась полная обратная совместимость.

--- services/payment/models/repositories/CurrencyRepository.php
+++ services/payment/models/repositories/CurrencyRepository.php
@@ -5,71 +5,106 @@ namespace app\services\payment\models\repositories;
 use app\services\payment\models\Currency;
 use yii\db\ActiveRecord;
 use yii\db\Query;
+use yii\helpers\ArrayHelper;
 
+/**
+ * CurrencyRepository
+ */
 class CurrencyRepository
 {
+    /**
+     * @var Currency[]
+     */
+    private static $currencies;
+    /**
+     * @var Currency[]
+     */
+    private static $currenciesById;
+    /**
+     * @var Currency[]
+     */
+    private static $currenciesByCode;
+
+    /**
+     * @return Currency[]
+     */
+    public static function getCurrencies(): array
+    {
+        if (!self::$currencies) {
+            self::$currencies = Currency::find()->all();
+        }
+        return self::$currencies;
+    }
+
+    /**
+     * @return Currency[]
+     */
+    public static function getCurrenciesById(): array
+    {
+        if (!self::$currenciesById) {
+            self::$currenciesById = ArrayHelper::index(self::getCurrencies(), 'Id');
+        }
+        return self::$currenciesById;
+    }
+
+    /**
+     * @return Currency[]
+     */
+    public static function getCurrenciesByCode(): array
+    {
+        if (!self::$currenciesByCode) {
+            self::$currenciesByCode = ArrayHelper::index(self::getCurrencies(), 'Code');
+        }
+        return self::$currenciesByCode;
+    }
+
+    public static function getCurrencyById(int $id): ?Currency
+    {
+        return self::getCurrenciesById()[$id] ?? null;
+    }
+
+    public static function getCurrencyByCode(string $code): ?Currency
+    {
+        return self::getCurrenciesByCode()[$code] ?? null;
+    }
+
     /**
      * @return Currency[]
      */
     public static function getAll(): array
     {
-        return Currency::find()->all();
+        return self::getCurrenciesById();
     }
 
-    /**
-     * @param int $id
-     * @return Currency|null
-     */
     public static function getCurrencyCodeById(int $id): ?Currency
     {
-        return Currency::findOne([
-            'Id' => $id
-        ]);
+        return self::getCurrencyById($id);
     }
-    /**
-     * @param string $currency
-     * @return bool
-     */
-    public function hasCurrency(string $currency): bool
+
+    public function hasCurrency(string $code): bool
     {
-        return Currency::find()
-            ->where([
-                'Code' => $currency
-            ])->exists();
+        return (bool)self::getCurrencyByCode($code);
     }
 
     /**
-     * @param string|null $currencyCode
-     * @param int|null $currencyId
+     * @param string|null $code
+     * @param int|null $id
+     *
      * @return null|Currency|Currency[]
      */
-    public function getCurrency(string $currencyCode = null, int $currencyId = null)
+    public function getCurrency(string $code = null, int $id = null)
     {
-        $query = Currency::find();
-        if (!$currencyCode && !$currencyId) {
-            return $query->all();
+        if (!$code && !$id) {
+            return self::getCurrenciesById();
         }
-        if ($currencyCode) {
-            $query->where([
-                'Code' => $currencyCode
-            ]);
-        }
-        if ($currencyId) {
-            $query->where([
-                'Id' => $currencyId
-            ]);
-        }
-        return $query->one();
+        $code = $code ? self::getCurrencyByCode($code) : $code;
+        $id = $id ? self::getCurrencyById($id) : $id;
+
+        return $code == $id ? $id : null;
     }
 
-    /**
-     * @return array|ActiveRecord|null
-     */
-    public function getDefaultMainCurrency()
+    public function getDefaultMainCurrency(): ?Currency
     {
-        return Currency::find()
-            ->where([
-                'Code' => Currency::MAIN_CURRENCY
-            ])->one();
+        return self::getCurrencyByCode(Currency::MAIN_CURRENCY);
     }
 }
