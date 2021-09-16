e11a2d598911535995c029438c4ae2be97ec9d34
Никита Черненко <nikitozina@inbox.ru>
2021-09-16 16:35:19 +0300

    VPBC-866. Phone и Fio в /out/check-sbp-can-transfer

--- services/payment/banks/BRSSbpTestData.php
+++ services/payment/banks/BRSSbpTestData.php
@@ -6,10 +6,10 @@ class BRSSbpTestData
 {
     public const ACCOUNT = '40702810700000007050';
     public const BIC = '044525151';
-    public const RECEIVER_ID = '0079167932356';
-    public const FIRST_NAME = 'Максим';
-    public const LAST_NAME = 'Филин';
-    public const MIDDLE_NAME = 'Сергеевич';
+    public const RECEIVER_ID = '0079661537198';
+    public const FIRST_NAME = 'Константин';
+    public const LAST_NAME = 'Петров';
+    public const MIDDLE_NAME = 'Юрьевич';
     public const MERCHANT_ID = 'MA0000086553';
     public const RECEIVER_ID_TYPE = 'MTEL';
 
