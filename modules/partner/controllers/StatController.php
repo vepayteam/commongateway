3432008a4aba461942813a10d0b8b690c193e9dc
Кирилл Большаков <kbolshakov@vepay.online>
2021-09-19 18:03:33 +0300

    feat: VPBC-855 ЛК / Доработка раздела Отчеты Правильный мёрдж

--- modules/partner/controllers/StatController.php
+++ modules/partner/controllers/StatController.php
@@ -291,11 +291,10 @@ class StatController extends Controller
     {
         $fltr = new StatFilter();
         $IsAdmin = UserLk::IsAdmin(Yii::$app->user);
-        $partnerId = $IsAdmin ? -1 : UserLk::getPartnerId(Yii::$app->user);
         return $this->render('otch', [
             'IsAdmin' => $IsAdmin,
             'partnerlist' => $fltr->getPartnersList(),
-            'magazlist' => $fltr->getMagazList($partnerId),
+            'magazlist' => $IsAdmin ? $fltr->getMagazList(-1) : $fltr->getMagazList(UserLk::getPartnerId(Yii::$app->user)),
             'uslugilist' => $fltr->getTypeUslugLiust(),
             'bankList' => $fltr->getBankList(),
         ]);
