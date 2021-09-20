bb4b7e52105f374fb517f588551c4108a7d35524
Кирилл Большаков <kbolshakov@vepay.online>
2021-09-20 13:23:11 +0300

    feat: VPBC-855 ЛК / Доработка раздела Отчеты Trait v2

--- models/ModelHelper.php
+++ models/ModelHelper.php
@@ -2,14 +2,54 @@
 
 namespace app\models;
 
-include '../vendor/autoload.php';
-
-use yii\base\Model;
-
+/**
+ *
+ */
 trait ModelHelper
 {
-    public function loadAndValidate($data, $formName = null, $validate = false, $attributeNamesToValidate = null, $clearValidateErrors = true)
+    /**
+     * @var
+     */
+    public $isTrue;
+
+    /**
+     * @param $data
+     * @param null $formName
+     * @param false $validate
+     * @param null $attributeNamesToValidate
+     * @param bool $clearValidateErrors
+     *
+     * @return bool
+     */
+    public function loadAndValidate($data, $formName = null, bool $validate = false, $attributeNamesToValidate = null, bool $clearValidateErrors = true)
     {
-        return parent::load($data, $formName) && $validate ? $this->validate() : true;
+        $loaded = parent::load($data, $formName);
+        return $validate ? $loaded && $loaded->validate($attributeNamesToValidate, $clearValidateErrors) : $loaded;
+    }
+
+    /**
+     * @param $data
+     * @param null $formName
+     *
+     * @return $this
+     */
+    public function load($data, $formName = null)
+    {
+        $this->isTrue = $this->isTrue ? parent::load($data, $formName) : false;
+
+        return $this;
+    }
+
+    /**
+     * @param bool $runValidation
+     * @param null $attributeNames
+     *
+     * @return $this
+     */
+    public function save(bool $runValidation = true, $attributeNames = null)
+    {
+        $this->isTrue = $this->isTrue ? parent::save($runValidation, $attributeNames) : false;
+
+        return $this;
     }
 }
