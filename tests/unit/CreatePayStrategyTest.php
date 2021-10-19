52ac2e2b1de9a7e324643b643babeae57ecc7759
npchelnikov <npchelnikov@vepay.online>
2021-10-19 11:52:40 +0300

    feature/VPBC-1019. Блокировка повторной отправки запроса на создание платежа

---
+++ tests/unit/CreatePayStrategyTest.php
@@ -0,0 +1,83 @@
+<?php
+
+use app\services\payment\exceptions\DuplicateCreatePayException;
+use app\services\payment\forms\CreatePayForm;
+use app\services\payment\models\PaySchet;
+use app\services\payment\payment_strategies\CreatePayStrategy;
+use yii\mutex\FileMutex;
+
+class CreatePayStrategyTest extends \Codeception\Test\Unit
+{
+    /** @var \UnitTester */
+    protected $tester;
+
+    public function testSuccessCheckCreatePayLock()
+    {
+        $paySchet = $this->make(PaySchet::class, [
+            'ID' => 1,
+        ]);
+
+        $createPayStrategy = $this->make(CreatePayStrategy::class, [
+            'mutex' => new FileMutex(),
+        ]);
+
+        $reflection = new ReflectionClass(CreatePayStrategy::class);
+
+        $getCacheKey = $reflection->getMethod('getCacheKey');
+        $getCacheKey->setAccessible(true);
+        $checkCreatePayLock = $reflection->getMethod('checkCreatePayLock');
+        $checkCreatePayLock->setAccessible(true);
+
+        $checkCreatePayLock->invoke($createPayStrategy, $paySchet);
+        $cacheKey = $getCacheKey->invoke($createPayStrategy, $paySchet);
+
+        $this->assertTrue(Yii::$app->cache->exists($cacheKey));
+    }
+
+    public function testFailCheckCreatePayLock()
+    {
+        $paySchet = $this->make(PaySchet::class, [
+            'ID' => 1,
+        ]);
+
+        $createPayStrategy = $this->make(CreatePayStrategy::class, [
+            'mutex' => new FileMutex(),
+        ]);
+
+        $reflection = new ReflectionClass(CreatePayStrategy::class);
+
+        $checkCreatePayLock = $reflection->getMethod('checkCreatePayLock');
+        $checkCreatePayLock->setAccessible(true);
+
+        $checkCreatePayLock->invoke($createPayStrategy, $paySchet);
+
+        $this->tester->expectThrowable(DuplicateCreatePayException::class, function () use ($checkCreatePayLock, $createPayStrategy, $paySchet) {
+            $checkCreatePayLock->invoke($createPayStrategy, $paySchet);
+        });
+    }
+
+    public function testReleaseLock()
+    {
+        $paySchet = $this->make(PaySchet::class, [
+            'ID' => 1,
+        ]);
+
+        $createPayStrategy = $this->construct(CreatePayStrategy::class, [new CreatePayForm(['IdPay' => $paySchet->ID])]);
+
+        $reflection = new ReflectionClass(CreatePayStrategy::class);
+
+        $getCacheKey = $reflection->getMethod('getCacheKey');
+        $getCacheKey->setAccessible(true);
+        $checkCreatePayLock = $reflection->getMethod('checkCreatePayLock');
+        $checkCreatePayLock->setAccessible(true);
+
+        $checkCreatePayLock->invoke($createPayStrategy, $paySchet);
+        $cacheKey = $getCacheKey->invoke($createPayStrategy, $paySchet);
+
+        $this->assertTrue(Yii::$app->cache->exists($cacheKey));
+
+        $createPayStrategy->releaseLock();
+
+        $this->assertFalse(Yii::$app->cache->exists($cacheKey));
+    }
+}
