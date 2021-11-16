4f320ba89c98764f7abfcdd088d6bf94737b7f66
Кирилл Большаков <kbolshakov@vepay.online>
2021-11-17 01:18:02 +0300

    VPBC-855 ЛК / Доработка раздела Отчеты

--- models/partner/stat/PayShetStat.php
+++ models/partner/stat/PayShetStat.php
@@ -493,7 +493,7 @@ class PayShetStat extends Model
                 ':DATETO' => strtotime($this->dateto . ":59")
             ])
             ->andWhere('ps.Status = 1')
-            ->groupBy('IdUsluga, bank, bankName, NameUsluga, IsCustom')
+            ->groupBy('ps.IdUsluga, b.Name, ps.Bank, NameUsluga, IsCustom')
             ->orderBy('bank')
         ;
 
