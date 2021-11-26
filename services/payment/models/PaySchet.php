f1abbed07468bc73a5b28cb623baf30ffcb42e02
Никита Черненко <nikitozina@inbox.ru>
2021-11-26 13:40:48 +0300

    VPBC-1148. Не передаем корректный callback_url в Форту

--- services/payment/models/PaySchet.php
+++ services/payment/models/PaySchet.php
@@ -462,6 +462,14 @@ class PaySchet extends \yii\db\ActiveRecord
     }
 
     /**
+     * @return string
+     */
+    public function getCallbackUrl(): string
+    {
+        return Yii::$app->params['domain'] . '/mfo/pay/callback';
+    }
+
+    /**
      * @param string|null $cardNumber
      * @return string
      */
