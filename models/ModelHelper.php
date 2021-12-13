57eea868b221e37bd42a5f884bff953689d8a7e8
Кирилл Большаков <kbolshakov@vepay.online>
2021-12-13 08:24:09 +0300

    fix: VPBC-1168 Неверная статистика в "Количество новых карт" fixed

--- models/ModelHelper.php
+++ models/ModelHelper.php
@@ -2,17 +2,24 @@
 
 namespace app\models;
 
+use app\models\partner\UserLk;
+use Yii;
 use yii\db\ActiveRecord;
 
 /**
  * @see ActiveRecord
+ * @property bool $isAdmin
  */
 trait ModelHelper
 {
     /**
-     * @var bool
+     * @proretry bool $loaded
      */
     public $loaded = false;
+    /**
+     * @proretry bool $_isAdmin
+     */
+    private $_isAdmin = null;
 
     /**
      * @param array $data
@@ -41,6 +48,19 @@ trait ModelHelper
         return $data ? parent::load($data, $formName) && parent::validate($attributeNames, $clearErrors) : parent::validate($attributeNames, $clearErrors);
     }
 
+    /**
+     * @param array $data
+     * @param string|null $formName
+     * @param null|string|array $attributeNames
+     * @param bool $clearErrors
+     *
+     * @return bool
+     */
+    public function loadAndValidate(array $data, ?string $formName = null, $attributeNames = null, bool $clearErrors = true): bool
+    {
+        return $this->load($data, $formName, true, $attributeNames, $clearErrors);
+    }
+
     public function isLoaded(): bool
     {
         return $this->loaded;
@@ -49,5 +69,11 @@ trait ModelHelper
     public function setLoaded(bool $loaded): void
     {
         $this->loaded = $loaded;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        $this->_isAdmin = $this->_isAdmin ?? (bool)UserLk::IsAdmin(Yii::$app->user);
+        return $this->_isAdmin;
     }
 }
