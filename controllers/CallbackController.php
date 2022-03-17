a1dfce6e78fc608e450541a05af5080cff04370b
Magomed Gadzhiev <magadzhiev@vepay.online>
2022-03-17 12:28:19 +0300

    fix: VPBC-1377 Endpoint для Impaya callback

---
+++ controllers/CallbackController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\api\CorsTrait;
+use app\services\callbacks\forms\ImpayaCallbackForm;
+use app\services\callbacks\ImpayaCallbackService;
+use Yii;
+use yii\helpers\Json;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\web\Response;
+
+class CallbackController extends Controller
+{
+    use CorsTrait;
+
+    /**
+     * @param $action
+     * @return bool
+     * @throws BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+        if ($this->checkBeforeAction()) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            $this->enableCsrfValidation = false;
+            return parent::beforeAction($action);
+        }
+        return false;
+    }
+
+    public function actionImpaya()
+    {
+        $impayaCallbackForm = new ImpayaCallbackForm();
+        if(!$impayaCallbackForm->load(Yii::$app->request->post(), '') || !$impayaCallbackForm->validate()) {
+            $log = sprintf('Callback Impaya error: %s, data=' . Yii::$app->request->post());
+            Yii::warning($log);
+            return $this->asJson(['status' => 0]);
+        } else {
+            $log = sprintf(
+                'Callback Impaya id=%d, status=%d, data=%s',
+                $impayaCallbackForm->mc_transaction_id,
+                $impayaCallbackForm->status_id,
+                Json::encode($impayaCallbackForm->getAttributes())
+            );
+            Yii::warning($log);
+            $impayaCallbackService = new ImpayaCallbackService();
+            $impayaCallbackService->execCallback($impayaCallbackForm);
+            return $this->asJson(['status' => 1]);
+        }
+    }
+
+}
