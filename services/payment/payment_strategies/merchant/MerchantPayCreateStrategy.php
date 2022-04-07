08a1ac89b8b3269a6f2be795435115535baac034
Никита Черненко <nikitozina@inbox.ru>
2022-04-07 10:17:52 +0300

    feat: VPBC-1268. Несоответствие времени жизни платежной формы значению timeout

--- services/payment/payment_strategies/merchant/MerchantPayCreateStrategy.php
+++ services/payment/payment_strategies/merchant/MerchantPayCreateStrategy.php
@@ -163,6 +163,9 @@ class MerchantPayCreateStrategy
         if (!$paySchet->save()) {
             throw new CreatePayException('Не удалось создать счет');
         }
+
+        Yii::info('id: ' . $paySchet->ID . ', timeout: ' . $this->payForm->timeout . ' minutes', 'mfo');
+
         return $paySchet;
     }
 
