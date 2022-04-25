feebd0a4bfb73ea442d925e315a5bd65de6c5091
Albert Garipov <agaripov@vepay.online>
2022-04-25 13:04:04 +0300

    feat: VPBC-1421 Удаление лишних индексов на таблице pay_schet

--- migrations/m220420_234248_drop_pay_schet_indexes.php
+++ migrations/m220420_234248_drop_pay_schet_indexes.php
@@ -13,7 +13,18 @@ class m220420_234248_drop_pay_schet_indexes extends Migration
     public function up()
     {
         $this->dropIndex('sms_accept_idx', 'pay_schet');
+        $this->dropIndex('idx_pay_schet_sms_accept', 'pay_schet');
         $this->dropIndex('ExtBillNumber', 'pay_schet');
+        $this->dropIndex('idx_pay_schet_idgroupoplat', 'pay_schet');
+        $this->dropIndex('pay_schet_status_datecreate_index', 'pay_schet');
+
+
+        $this->dropIndex('idx_by_rsbcron', 'pay_schet');
+        $this->createIndex('idx_by_rsbcron', 'pay_schet', [
+            'Status',
+            'DateLastUpdate',
+            'ExtBillNumber',
+        ]);
     }
 
     /**
@@ -21,7 +32,19 @@ class m220420_234248_drop_pay_schet_indexes extends Migration
      */
     public function down()
     {
+
+        $this->dropIndex('idx_by_rsbcron', 'pay_schet');
+        $this->createIndex('idx_by_rsbcron', 'pay_schet', [
+            'Status',
+            'DateLastUpdate',
+            'ExtBillNumber',
+            'sms_accept',
+        ]);
+
+        $this->createIndex('pay_schet_status_datecreate_index', 'pay_schet', ['Status', 'DateCreate']);
+        $this->createIndex('idx_pay_schet_idgroupoplat', 'pay_schet', 'IdGroupOplat');
         $this->createIndex('ExtBillNumber', 'pay_schet', 'ExtBillNumber');
+        $this->createIndex('idx_pay_schet_sms_accept', 'pay_schet', 'sms_accept');
         $this->createIndex('sms_accept_idx', 'pay_schet', 'sms_accept');
     }
 }
