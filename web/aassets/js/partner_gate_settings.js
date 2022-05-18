1fa6ba3e5fe89be9b44cf90e69ce64d456e5849c
Barsegyan <gbarseghyan@vepay.online>
2022-05-18 22:06:31 +0300

    fix: VPBC-1368-fix_gate_comission_form_v2 remove assets deps

--- web/aassets/js/partner_gate_settings.js
+++ web/aassets/js/partner_gate_settings.js
@@ -1,28 +1,36 @@
-const form = document.querySelector('#partner-edit__bank-gates-edit-modal__gate-form')
-const fixedComissionCurrency = form.querySelector('select[name=FeeCurrencyId]')
-const minimalComissionCurrency = form.querySelector('select[name=MinimalFeeCurrencyId]')
 
-fixedComissionCurrency.addEventListener('change', function (e) {
-    try {
+if (document.querySelector('#partner-edit__bank-gates-edit-modal__gate-form')) {
+    const form = document.querySelector('#partner-edit__bank-gates-edit-modal__gate-form')
+    const fixedComissionCurrency = form.querySelector('select[name=FeeCurrencyId]')
+    const minimalComissionCurrency = form.querySelector('select[name=MinimalFeeCurrencyId]')
+
+    function updateCurrency(e, elements) {
+        try {
+            const selectedCurrency = e.target.options[e.target.options.selectedIndex]
+            let currentCurrency;
+            if (selectedCurrency) {
+                currentCurrency = selectedCurrency.innerText
+            }
+            if (elements) {
+                elements.forEach(function (el) {
+                    el.textContent = currentCurrency || ''
+                })
+            }
+        } catch (e) {
+            console.log('ошибка изменения валюты', e)
+        }
+
+
+    }
+
+    fixedComissionCurrency.addEventListener('change', function (e) {
         const chosenCurrencyElements = form.querySelectorAll('#comissionWrapper .fixedComissionCurrency')
-        const currentCurrency = e.target.options[e.target.options.selectedIndex]?.innerText
-        chosenCurrencyElements.forEach(function (el) {
-            el.textContent = currentCurrency || ''
-        } )
-    } catch (e) {
-        console.log('ошибка изменения валюты', e)
-    }
-})
+        updateCurrency(e, chosenCurrencyElements)
+    })
 
-minimalComissionCurrency.addEventListener('change', function (e) {
-    try {
+    minimalComissionCurrency.addEventListener('change', function (e) {
         const chosenCurrencyElements = form.querySelectorAll('#comissionWrapper .minimalComissionCurrency')
-        const currentCurrency = e.target.options[e.target.options.selectedIndex]?.innerText
-        chosenCurrencyElements.forEach(function (el) {
-            el.textContent = currentCurrency || ''
-        } )
-    } catch (e) {
-        console.log('ошибка изменения валюты', e)
-    }
-})
+        updateCurrency(e, chosenCurrencyElements)
+    })
 
+}
