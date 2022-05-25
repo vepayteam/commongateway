fb5ede483b665c10e8a867efae8c6b0e74f0c757
Barsegyan <gbarseghyan@vepay.online>
2022-05-25 12:25:13 +0300

    fix: VPBC-1368 update currency on edit
    Signed-off-by: Barsegyan <gbarseghyan@vepay.online>

--- web/aassets/js/partner_gate_settings.js
+++ web/aassets/js/partner_gate_settings.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     const gateForm = document.querySelector('#partner-edit__bank-gates-edit-modal__gate-form')
-    
+
     if (gateForm) {
 
         const fixedComissionCurrency = gateForm.querySelector('select[name=FeeCurrencyId]')
         const minimalComissionCurrency = gateForm.querySelector('select[name=MinimalFeeCurrencyId]')
+        const fixedCurrencyElements = gateForm.querySelectorAll('#comissionWrapper .fixedComissionCurrency')
+        const minimalCurrencyElements = gateForm.querySelectorAll('#comissionWrapper .minimalComissionCurrency')
 
-        function updateCurrency(e, elements) {
+        function getSelectedCurrency (dropDown) {
+            const selectedCurrency = dropDown.options[dropDown.options.selectedIndex]
+            return selectedCurrency
+        }
+
+        function updateCurrency(selectedCurrency, elements) {
             try {
-                const selectedCurrency = e.target.options[e.target.options.selectedIndex]
                 let currentCurrency;
                 if (selectedCurrency) {
                     currentCurrency = selectedCurrency.innerText
@@ -25,14 +31,40 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         fixedComissionCurrency.addEventListener('change', function (e) {
-            const chosenCurrencyElements = gateForm.querySelectorAll('#comissionWrapper .fixedComissionCurrency')
-            updateCurrency(e, chosenCurrencyElements)
+            const selectedCurrency = getSelectedCurrency(e.target)
+            updateCurrency(selectedCurrency, fixedCurrencyElements)
         })
 
         minimalComissionCurrency.addEventListener('change', function (e) {
-            const chosenCurrencyElements = gateForm.querySelectorAll('#comissionWrapper .minimalComissionCurrency')
-            updateCurrency(e, chosenCurrencyElements)
+            const selectedCurrency = getSelectedCurrency(e.target)
+            updateCurrency(selectedCurrency, minimalCurrencyElements)
         })
+
+        const addGateBtn = document.querySelector('#partner-edit__bank-gates-table__add-button')
+        if (addGateBtn) {
+            addGateBtn.addEventListener('click', function () {
+                // очищаем поля при добавлении нового шлюза
+                updateCurrency({}, fixedCurrencyElements)
+                updateCurrency({}, minimalCurrencyElements)
+
+            })
+        }
+
+        const editGateBtns = document.querySelectorAll('.partner-edit__bank-gates-table__edit-button')
+        if (editGateBtns) {
+            editGateBtns.forEach(function (button) {
+                button.addEventListener('click', function () {
+                    setTimeout(function () {
+                        const fixedCurrency = getSelectedCurrency(fixedComissionCurrency)
+                        const minimalCurrency = getSelectedCurrency(minimalComissionCurrency)
+
+                        updateCurrency(fixedCurrency, fixedCurrencyElements)
+                        updateCurrency(minimalCurrency, minimalCurrencyElements)
+                        // ждем обновления selectedIndex 100 миллисекунд, иначе не успевает отобразиться информация
+                    }, 100)
+                })
+            })
+        }
     }
 
 });
