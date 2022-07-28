017aa4d2913f546fc47dda51d63a71d5a36352cf
npchelnikov <npchelnikov@vepay.online>
2022-07-28 11:39:24 +0300

    fix: VPBC-1536 Добавить настройки языка в остальные конфиги

--- config/test.php
+++ config/test.php
@@ -12,7 +12,7 @@ $db = require(__DIR__ . '/test_db.php');
 return [
     'id' => 'basic-tests',
     'basePath' => dirname(__DIR__),
-    'language' => 'ru-RU',
+    'language' => 'en-US',
     'sourceLanguage' => 'ru-RU',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
